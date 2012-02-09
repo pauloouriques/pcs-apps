1bdde649328a4065ce5e82570846ff189320db63
ourixilva <ourixilva@87ad5495-f94a-cda4-b5c0-e31b5b965ff4>
2012-02-09 04:38:25 +0000

    Bug da tada resolvido e a activity Details criada.

--- MessageList/src/org/developerworks/android/MessageList.java
+++ MessageList/src/org/developerworks/android/MessageList.java
@@ -1,21 +1,32 @@
 package org.developerworks.android;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.xmlpull.v1.XmlSerializer;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Xml;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MessageList extends ListActivity {
 	
@@ -28,12 +39,10 @@ public class MessageList extends ListActivity {
         loadFeed();
     }
     
-	@SuppressWarnings("unchecked")
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		super.onMenuItemSelected(featureId, item);
-		ArrayAdapter<String> adapter =
-			(ArrayAdapter<String>) this.getListAdapter();
+		ArrayAdapter<Message> adapter = new MessageItemAdapter(getApplicationContext(), R.layout.listitem, this.messages);
 		if (adapter.getCount() > 0){
 			adapter.clear();
 		}
@@ -44,9 +53,13 @@ public class MessageList extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		Intent viewMessage = new Intent(Intent.ACTION_VIEW, 
-				Uri.parse(messages.get(position).getLink().toExternalForm()));
-		this.startActivity(viewMessage);
+		Intent intent = new Intent(this, Details.class);
+		intent.putExtra("link", messages.get(position).getLink().toExternalForm());
+		intent.putExtra("title", messages.get(position).getTitle());
+		intent.putExtra("date", messages.get(position).getDate().toString());
+		intent.putExtra("imgSrc", parseImg(messages.get(position).getDescription()));
+		intent.putExtra("subtitle", parseSubtitle(messages.get(position).getDescription()));
+    	startActivity(intent);
 	}
 
 	private void loadFeed(){
@@ -61,10 +74,11 @@ public class MessageList extends ListActivity {
 	    	Log.i("AndroidNews", xml);
 	    	List<String> titles = new ArrayList<String>(messages.size());
 	    	for (Message msg : messages){
-	    		titles.add(msg.getTitle());
+	    		if (msg.getTitle().length() < 200)
+	    			titles.add(msg.getTitle());
 	    	}
-	    	ArrayAdapter<String> adapter = 
-	    		new ArrayAdapter<String>(this, R.layout.row,titles);
+	    	this.messages.remove(0);
+	    	MessageItemAdapter adapter = new MessageItemAdapter(getApplicationContext(), R.layout.listitem, this.messages);
 	    	this.setListAdapter(adapter);
     	} catch (Throwable t){
     		Log.e("AndroidNews",t.getMessage(),t);
@@ -100,4 +114,73 @@ public class MessageList extends ListActivity {
 			throw new RuntimeException(e);
 		} 
 	}
+	
+	public class MessageItemAdapter extends ArrayAdapter<Message> {
+		private List<Message> messages;
+
+		public MessageItemAdapter(Context context, int textViewResourceId, List<Message> messages) {
+			super(context, textViewResourceId, messages);
+			this.messages = messages;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if (v == null) {
+				LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = vi.inflate(R.layout.listitem, null);
+			}
+
+			Message message = messages.get(position);
+			if (message != null) {
+				TextView title = (TextView) v.findViewById(R.id.title);
+				TextView date = (TextView) v.findViewById(R.id.date);
+				ImageView image = (ImageView) v.findViewById(R.id.image);
+
+				if (title != null) {
+					title.setText(message.getTitle());
+				}
+
+				if(date != null) {
+					date.setText(message.getDate());
+				}
+				
+				if(image != null) {
+					if (!parseImg(message.getDescription()).equals("http://globoesporte.globo.com")){
+						image.setImageBitmap(getImageBitmap(parseImg(message.getDescription())));
+					}
+				}
+			}
+			return v;
+		}
+	}
+	
+	Bitmap getImageBitmap(String url) {
+        Bitmap bm = null; 
+        try { 
+            URL aURL = new URL(url); 
+            URLConnection conn = aURL.openConnection(); 
+            conn.connect(); 
+            InputStream is = conn.getInputStream(); 
+            BufferedInputStream bis = new BufferedInputStream(is); 
+            bm = BitmapFactory.decodeStream(bis); 
+            bis.close(); 
+            is.close(); 
+       } catch (IOException e) { 
+           Log.e("ERRO AQUI", "Error getting bitmap", e); 
+       } 
+       return bm; 
+    } 
+	
+	public String parseImg(String description){
+		String aux = description.substring(description.indexOf("src='") + 5);
+		String imgPath = aux.substring(0, aux.indexOf("'"));
+		return imgPath;
+	}
+	
+	public String parseSubtitle(String description){
+		String imgPath = description.substring(description.indexOf("</a><br />") + 10);
+		return imgPath;
+	}
+	
 }
---
+++ MessageList/src/org/developerworks/android/utils/Item.java
@@ -0,0 +1,16 @@
+package org.developerworks.android.utils;
+
+public class Item{
+	
+    public final String text;
+    public final int icon;
+    
+    public Item(String text, Integer icon) {
+        this.text = text;
+        this.icon = icon;
+    }
+    @Override
+    public String toString() {
+        return text;
+    }
+}
