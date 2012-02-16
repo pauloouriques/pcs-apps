a1a02d0730e3358387b4b5ada9d1092a967b852b
ourixilva <ourixilva@87ad5495-f94a-cda4-b5c0-e31b5b965ff4>
2012-02-16 04:46:00 +0000

    Alteração em uma exceção.

--- InfoGalo/src/br/pb/pcs/infogalo/FeedParser.java
+++ InfoGalo/src/br/pb/pcs/infogalo/FeedParser.java
@@ -1,6 +1,7 @@
 package br.pb.pcs.infogalo;
+import java.net.UnknownHostException;
 import java.util.List;
 
 public interface FeedParser {
-	List<Message> parse();
+	List<Message> parse() throws UnknownHostException;
 }
