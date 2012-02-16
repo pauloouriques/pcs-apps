0753831c3ad396fc40142ae7b4470440c4c26f26
ourixilva <ourixilva@87ad5495-f94a-cda4-b5c0-e31b5b965ff4>
2012-02-16 04:46:41 +0000

    :D

--- MessageList/src/br/pb/pcs/inforaposa/SaxFeedParser.java
+++ MessageList/src/br/pb/pcs/inforaposa/SaxFeedParser.java
@@ -1,5 +1,6 @@
 package br.pb.pcs.inforaposa;
 
+import java.net.UnknownHostException;
 import java.util.List;
 
 import javax.xml.parsers.SAXParser;
@@ -11,13 +12,15 @@ public class SaxFeedParser extends BaseFeedParser {
 		super(feedUrl);
 	}
 	
-	public List<Message> parse() {
+	public List<Message> parse() throws UnknownHostException {
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
 			SAXParser parser = factory.newSAXParser();
 			RssHandler handler = new RssHandler();
 			parser.parse(this.getInputStream(), handler);
 			return handler.getMessages();
+		} catch (UnknownHostException e) {
+			throw new UnknownHostException();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		} 
